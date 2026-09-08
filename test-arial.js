05dfc99094dddaf94b36f6313ce97c4df2648631
Yassin Mokni <yassin.mokni@gmail.com>
2026-09-08 15:28:35 +0200

    chore: remove temporary SVG generation and test scripts

--- test-arial.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-let svg = fs.readFileSync('/tmp/clean_og.svg', 'utf8');
-svg = svg.replace(/font-family:system-ui[^;]+;/g, 'font-family:-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;');
-fs.writeFileSync('/tmp/clean_og_arial.svg', svg);
